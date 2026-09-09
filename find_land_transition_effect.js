741b4c9b36192226e4cc534fd7a51e83d31cf73f
Amitiel <armitiel@gmail.com>
2026-09-09 14:07:09 +0200

    Porzadki: usuniete stare skrypty pomocnicze, kopie bundla sceny i pliki testowe; zrodla graficzne poza public

--- find_land_transition_effect.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const f = fs.readFileSync('C:/Users/DELL/EwolucJA/frontend/src/components/LandTransition.jsx', 'utf8');
-const idx = f.indexOf('useEffect(() => {');
-if (idx > -1) {
-  console.log(f.substring(idx, idx + 500));
-}
